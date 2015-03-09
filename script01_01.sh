219edd54d9a85fd9474c18a94d61e20741a8831d
mfalcinelli <falcio97@gmail.com>
2015-03-09 19:03:55 +0100

    Update script01_01.sh

--- script01_01.sh
+++ script01_01.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+if [ $# -gt 1 -o $# -eq 0 ]
+then
+	echo
+	echo "Inserire  un  argomento!"
+	exit 1
+fi
+
+echo
+if [ -e $@ ]
+then
+	echo "File trovato!"
+else
+	echo "File_non_trovato"
+fi
+
+exit 0
+
 
 # Riceve un argomento da riga di comando.
 # Se la directory corrente contiene il file il cui nome e' passato come argomento, stampa "File trovato!", altrimenti stampa "File _non_ trovato"
